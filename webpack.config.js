6629eaf9cd6113e075d6df8edc96ff4881630ae2
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2024-11-16 15:04:20 +0300

    upd dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,46 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    main: './src/index.js'
   },
-
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '',
+  },
+  mode: 'development',
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    open: true,
+    compress: true,
+    port: 8080
+  },
   module: {
     rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            use: ['svg-inline-loader'],
+        },
+        {
+            test: /\.css$/, use: [ 'style-loader', 'css-loader' ]
+        },
+        {
+            test: /\.(png|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource',
+        },
+        
+    ]
   },
-  
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: './src/index.html'
     }),
-  ],
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
 
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist'),
-    },
-    open: true,
-  },
-
-  mode: 'development',
-};
+  ]
+}
