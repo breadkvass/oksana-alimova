56b548d73fb0941e1283fa1d33863aae611b30f0
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-01-16 13:50:30 +0300

    add smooth opacity scrolling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './index.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+    let blocks = document.querySelectorAll('.block');
+ 
+    function checkBlocksVisibility() {
+        let windowHeight = window.innerHeight;
+ 
+        blocks.forEach(block => {
+            let blockPosition = block.getBoundingClientRect().top;
+ 
+            if (blockPosition < windowHeight - 100) {
+                block.style.opacity = "1";
+                block.style.transform = "translateY(0)";
+            }
+        })
+    }
+ 
+    checkBlocksVisibility();
+ 
+    window.addEventListener('scroll', function() {
+        checkBlocksVisibility();
+    });
+});
